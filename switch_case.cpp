7f4ae093046d00c37a39eadf81c4b7d48dcf96fd
Satyam Gupta <satyamsamguptasg@gmai.com>
2026-05-08 21:43:35 +0530

    switch with break

--- switch_case.cpp
+++ switch_case.cpp
@@ -19,21 +19,22 @@ while (true)
     cin >> op;
 
     switch (op) {
-        // If we not put break here, then it will execute all the cases from it finds the match case.
+        // If we not put break here, then it will execute all the cases from it finds the match case 
         case '&':
             cout << "The bitwise AND of " << a << " and " << b << " is: " << (a & b) << endl;
-            ;
+            break;
         case '|':
             cout << "The bitwise OR of " << a << " and " << b << " is: " << (a | b) << endl;
-            ;
+            break;     
         case '^' :
             cout << "The bitwise XOR of " << a << " and " << b << " is: " << (a ^ b) << endl;
-            ;
+            break;
         case '~' :
             cout << "The bitwise NOT of " << a << " is: " << (~a) << " and the bitwise NOT of " << b << " is: " << (~b) << endl;
-            ;
+            break;
         default :
             cout << "Invalid operator. Please choose a valid bitwise operator." << endl;
+            break;;
         }
 }
 }
